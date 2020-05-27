b94725bdab676ee59bc40bbe05cc05ce6547c5a4
Deyvi Tineo <deyvitineo@gmail.com>
2020-05-27 10:32:08 -0400

    Added timer to exercise activity and a constants file to hold constant values

--- app/src/main/java/com/deyvitineo/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/deyvitineo/a7minuteworkout/ExerciseActivity.kt
@@ -1,11 +1,22 @@
 package com.deyvitineo.a7minuteworkout
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.deyvitineo.a7minuteworkout.util.Constants
 
 class ExerciseActivity : AppCompatActivity() {
 
     private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var mProgressBar: ProgressBar
+    private lateinit var mTimerTextView: TextView
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,10 +24,13 @@ class ExerciseActivity : AppCompatActivity() {
 
         initWidgets()
         initActionBar()
+        setupRestView()
     }
 
     private fun initWidgets() {
         mToolbar = findViewById(R.id.toolbar_exercise_activity)
+        mProgressBar = findViewById(R.id.exercise_progress_bar)
+        mTimerTextView = findViewById(R.id.tv_timer)
     }
 
     private fun initActionBar() {
@@ -30,5 +44,37 @@ class ExerciseActivity : AppCompatActivity() {
 
     private fun initListeners() {
 
+    }
+
+    private fun setRestProgressBar(){
+        mProgressBar.progress = restProgress
+
+        restTimer = object : CountDownTimer(Constants.EXERCISE_EASY, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                var timeLeft = (Constants.EXERCISE_EASY / Constants.SECOND_IN_MILLIS) - restProgress
+                mProgressBar.progress = timeLeft.toInt()
+                mTimerTextView.text = timeLeft.toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Time has finished", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        if(restTimer != null){
+            restTimer!!.cancel()
+        }
+        super.onDestroy()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
     }
 }
---
+++ app/src/main/java/com/deyvitineo/a7minuteworkout/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.deyvitineo.a7minuteworkout.util
+
+object Constants {
+    const val EXERCISE_EASY: Long = 10000
+    const val EXERCISE_MEDIUM: Long = 30000
+    const val EXERCISE_HARD: Long = 60000
+    const val EXERCISE_ATHLETE: Long = 120000
+    const val SECOND_IN_MILLIS: Long = 1000
+}
